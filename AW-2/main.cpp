f107375eb6022d9d887aaf12a8d57e99079000b2
Arman Ashrafian <ashy63@gmail.com>
2018-01-18 17:16:39 -0800

    c++98 std

--- AW-2/main.cpp
+++ AW-2/main.cpp
@@ -9,32 +9,31 @@
 
 using namespace std;
 
-map<char, string> hex_map =
-{
-    {'0', "0000"},
-    {'1', "0001"},
-    {'2', "0010"},
-    {'3', "0011"},
-    {'4', "0100"},
-    {'5', "0101"},
-    {'6', "0110"},
-    {'7', "0111"},
-    {'8', "1000"},
-    {'9', "1001"},
-    {'A', "1010"},
-    {'B', "1011"},
-    {'C', "1100"},
-    {'D', "1101"},
-    {'E', "1110"},
-    {'F', "1111"},
-};
-
 string hexToBinary(string input) {
     string output;
+	map<char, string> hex_map;
+	
+	hex_map['0'] = "0000";
+	hex_map['1'] = "0001";
+	hex_map['2'] = "0010";
+	hex_map['3'] = "0011";
+	hex_map['4'] = "0100";
+	hex_map['5'] = "0101";
+	hex_map['6'] = "0110";
+	hex_map['7'] = "0111";
+	hex_map['8'] = "1000";
+	hex_map['9'] = "1001";
+	hex_map['A'] = "1010";
+	hex_map['B'] = "1011";
+	hex_map['C'] = "1100";
+	hex_map['D'] = "1101";
+	hex_map['E'] = "1110";
+	hex_map['F'] = "1111";
 
-    for(char c : input) {
-        output += hex_map[c];
-    }
+
+	for(int i = 0; i < input.size(); i++) {
+		output += hex_map[input[i]];
+	}
 
     return output;
 }
