e9a30ace55f15b2f17d3b7a6b66da7f89dfaadfc
Arman Ashrafian <ashy63@gmail.com>
2018-01-22 15:39:31 -0800

    convert binary str to hex str

--- AW-3/main.cpp
+++ AW-3/main.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <map>
 #include "math.h"
 
 using namespace std;
@@ -45,12 +46,39 @@ string intToBinary(int num) {
 }
 
 string binToHex(string bin) {
-    // @TODO
+    if(bin.size() % 4 != 0) { return ""; } // ensure multiple of 4
+
+    string out;
+    map<string, char> hexMap;
+
+    hexMap["0000"] = '0';
+    hexMap["0001"] = '1';
+    hexMap["0010"] = '2';
+    hexMap["0011"] = '3';
+    hexMap["0100"] = '4';
+    hexMap["0101"] = '5';
+    hexMap["0110"] = '6';
+    hexMap["0111"] = '7';
+    hexMap["1000"] = '8';
+    hexMap["1001"] = '9';
+    hexMap["1010"] = 'A';
+    hexMap["1011"] = 'B';
+    hexMap["1100"] = 'C';
+    hexMap["1101"] = 'D';
+    hexMap["1110"] = 'E';
+    hexMap["1111"] = 'F';
+
+    for(int i = 0; i < bin.size(); i+=4) {
+        out += hexMap[bin.substr(i, i+4)];
+    }
+
+    return out;
 }
 
 int main() {
 
-    cout << intToBinary(5) << endl;
+    // cout << intToBinary(5) << endl;
+    cout << binToHex("00010100") << endl;
 
     return 0;
 }
