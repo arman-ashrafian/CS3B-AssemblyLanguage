59f0768af2b219c6274ae7b6303df64a6abdf756
arman-ashrafian <ashy63@gmail.com>
2018-04-01 23:04:25 -0700

    add string_equals procedure

--- MASM3/MASM3.asm
+++ MASM3/MASM3.asm
@@ -18,10 +18,11 @@
 
     ; External Procedures
     extern String_length@0:PROC
+    extern String_equals@0:PROC
 
     ; Symplify External Procedure Names
     String_length equ String_length@0
-
+    String_equals equ String_equals@0
 ; Data Segment
     .data
 
@@ -46,12 +47,18 @@
     strStringStarts1  byte    "<10> String_startsWith_1       ",0
     strStringStarts2  byte    "<11> String_startsWith_2       ",0
     strStringEndWith  byte    "<12> String_endWith            ",0
+    strPrompt1        byte    "String 1: ",0
+    strPrompt2        byte    "String 2: ",0
     strCurrently      byte    "currently: ",0
     strNull           byte    "NULL",0
     strHoldInt        byte    10 dup(?)
     strTrue           byte    "True",0
     strFalse          byte    "False",0
-    
+
+    ; user input (bufffer size - 50 bytes)
+    strUserInput1     byte   50 dup(?)
+    strUserInput2     byte   50 dup(?)
+
     ; testing data
     strMyName byte "Arman",0
 
@@ -87,6 +94,13 @@ PrintMenu PROC
     invoke putstring, addr strStringEndWith 
     call NewLine
     invoke putstring, addr strStars
+    call NewLine
+    invoke putstring, addr strPrompt1
+    invoke getstring, addr strUserInput1, 50
+    call NewLine
+    invoke putstring, addr strPrompt2
+    invoke getstring, addr strUserInput2, 50
+    call NewLine
     ret
 PrintMenu ENDP
 
@@ -103,6 +117,13 @@ _start:
     mov eax, 0                                      ; for OllyDebug
     call Clrscr                                     ; clear screen
     call PrintMenu
+
+
+    push offset strUserInput2
+    push offset strUserInput1
+    call String_equals
+    call DumpRegs
+
     invoke ExitProcess, 0
 
 end _start                                          ; end program
--- MASM3/StringLibrary.asm
+++ MASM3/StringLibrary.asm
@@ -19,23 +19,80 @@
 .code 
 
 String_length PROC
-	push ebp					;preserve base register
-	mov ebp,esp					;set new stack frame
-	push ebx					;preserve used registers
-	push esi
-	mov ebx,[ebp+8]				;set ebx pointer to first string
-	mov esi,0					;move esi indexes into the string
-stLoop:
-    cmp byte ptr[ebx+esi],0	    ;denotes reaching the end of the string
-    je finished				    ;when yes, string input ends here
-    inc esi					    ;otherwise, continue to next character`
-    jmp stLoop				    ;until a NULL character is hit
-finished:
-	mov eax,esi					;returns the length to EAX
-	pop esi			            ;restore preserved registers
-	pop ebx			            ;restore preserved registers
-	pop ebp			            ;restore preserved registers
-	RET                         ;returns result from eax
-String_length ENDP              ;end String_length function
+    push ebp					; preserve base register
+    mov ebp,esp					; set new stack frame
+    push ebx					; preserve used registers
+    push esi 
+    mov ebx,[ebp+8]				; set ebx pointer to string
+    mov esi,0					; move esi indexes into the string
+stLoop: 
+    cmp byte ptr[ebx+esi],0	    ; reached the end of the string
+    je finished				    ; when yes, string input ends here
+    inc esi					    ; otherwise, continue to next character`
+    jmp stLoop				    ; until a NULL character is hit
+finished: 
+    mov eax,esi					; returns the length to EAX
+    pop esi			            ; restore preserved registers
+    pop ebx			            ; restore preserved registers
+    pop ebp			            ; restore preserved registers
+    RET                         ; returns result from eax
+String_length ENDP              ; end String_length function
+
+;*******************************************************
+; String_equals(string1:String,string2:String):boolean
+; - compares string 1 & string 2 byte-by-byte
+;*******************************************************
+String_equals PROC
+    push ebp              ; preserve base pointer
+    mov ebp, esp          ; new stack frame
+    sub esp, 8            ; make room for two dwords (8 bytes)
+    len1 equ [ebp - 4]    ; location on stack to store string 1 length
+    len2 equ [ebp - 8]    ; location on stack to store string 2 length
+    push ebx              ; preserve base pointer
+    push edi              ; preserve edi
+    push esi              ; preserve esi
+    mov ebx, [ebp + 8]    ; ebx = first string
+    mov esi, [ebp + 12]   ; esi = second string
+
+    ; determine length of strings
+    push ebx            ; string 1 address
+    call String_length
+    mov len1, eax       ; len1 = string 1 length
+    add esp, 4          ; restore stack pointer
+    push esi            ; esi = second string
+    call String_length
+    mov len2, eax       ; len2 = string 2 length
+    add esp, 4          ; restore stack pointer
+
+    ; compare string lengths
+    mov eax, len1
+    cmp eax, len2
+    jne notEqual
+
+    ; compare string byte-by-byte
+    mov edi, 0
+cmpLoop:
+    mov al, [ebx + edi]     ; char at current pos in string 1
+    cmp al, [esi + edi]     ; string1[pos] == string2[pos] ?
+    jne notEqual
+    cmp al, 0               ; check if end of string
+    je isEqual
+    inc edi                 ; no so increment string index
+    jmp cmpLoop
+
+notEqual:
+    mov al, 0   ; return false
+    jmp return
+isEqual:
+    mov al, 1
+return:
+    add esp, 8      ; restore stack used by local variables
+    pop esi         ; restore preserved registers
+    pop edi
+    pop ebx
+    pop ebp
+    ret 8           ; clear stack
+String_equals ENDP
+
 
 END
