69af02b72cdf2404b803d39d4a7cb72865aff3dd
arman-ashrafian <ashy63@gmail.com>
2018-04-26 16:12:28 -0700

    finish aw-16

---
+++ AW-16/main.asm
@@ -0,0 +1,29 @@
+;***************************************************************************
+; Program Name: main.asm
+; Programmer:   Arman Ashrafian
+; Class:        CS 3B
+; Date:         4-24-2018
+; Purpose:      AW-16
+;**************************************************************************
+
+; Include Irvine Lib
+INCLUDE    ..\..\Irvine\Irvine32.inc  ; Irvine Macros
+INCLUDE    ..\..\Irvine\Macros.inc
+INCLUDE    ..\macros\Bailey.inc       ; Bailey Macros
+
+mWriteAt MACRO X,Y,literal
+	mGotoxy X,Y
+	mWrite literal
+ENDM
+
+.data
+
+.code
+
+_start:	
+    call Clrscr
+    mWriteAt 20,20,"Location (20,20)"
+    call Crlf
+
+    invoke ExitProcess, 0
+end _start
